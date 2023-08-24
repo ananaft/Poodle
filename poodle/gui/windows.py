e2f673242f6f8cc4867de31800bfa80158c19de4
Lukas Hoettges <lukas.hoettges@gmx.de>
2023-08-24 13:16:35 +0200

    Removed old overview() and Tkinter dependency

--- poodle/gui/windows.py
+++ poodle/gui/windows.py
@@ -1,7 +1,7 @@
 # Gtk modules
 import gi
 gi.require_version('Gtk', '3.0')
-gi.require_version('WebKit2', '4.1')
+gi.require_version('WebKit2', '4.0')
 from gi.repository import Gtk, WebKit2
 # Poodle modules
 from poodle import config
